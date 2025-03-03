40621c637a04b3deb6c2883315988e53f360bbc7
MasudAlamMolla99 <masudalammolla99@gmail.com>
2025-03-03 13:53:13 +0530

    Review Schema added

---
+++ models/reviews.js
@@ -0,0 +1,38 @@
+import mongoose, { Schema } from "mongoose";
+
+const reviewSchema = new Schema(
+  {
+    full_name: {
+      type: String,
+      required: [true, "Name is required"],
+      trim: true,
+    },
+    email: {
+      type: String,
+      required: [true, "Email is required"],
+      trim: true,
+      lowercase: true,
+      match: [/\S+@\S+\.\S+/, "Please enter a valid email address"],
+    },
+    feedback: {
+      type: String,
+      trim: true,
+    },
+    image_url: {
+      type: String,
+      required: [true, "Room image URL is required"],
+    },
+    rating: {
+      type: Number,
+      required: [true, "Rating is required"],
+      min: [1, "Rating must be at least 1"],
+      max: [5, "Rating cannot exceed 5"],
+    },
+    pg_id: {
+      type: mongoose.Schema.Types.ObjectId,
+      ref: PgInfo,
+    },
+  },
+  { timestamps: true }
+);
+export const Review_Model = mongoose.model("Review", reviewSchema);
