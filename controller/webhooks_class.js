7523bfd82279f2aa510699b572be8f5ecf7c7d85
itzarkabrata <arkabrata1292@gmail.com>
2025-11-30 15:41:48 +0530

    Payment invoice endpoint done

--- controller/webhooks_class.js
+++ controller/webhooks_class.js
@@ -3,6 +3,7 @@ import { ApiResponse } from "../server-utils/ApiResponse.js";
 import { Database } from "../lib/connect.js";
 import { ApiError, InternalServerError } from "../server-utils/ApiError.js";
 import { Payment } from "./payment_class.js";
+import mongoose from "mongoose";
 
 const stripe = new Stripe(process.env.STRIPE_SECRET_KEY, {
   apiVersion: "2023-10-16",
@@ -10,16 +11,14 @@ const stripe = new Stripe(process.env.STRIPE_SECRET_KEY, {
 
 export class Webhooks {
   static async handleStripeWebhook(req, res) {
+    const session = await mongoose.startSession();
+    session.startTransaction();
     try {
       if (!Database.isConnected()) {
         throw new InternalServerError(
           "Database server is not connected properly"
         );
       }
-
-      // console.log("Webhook Called======");
-
-      // console.log("Request", req);
 
       const sig = req.headers["stripe-signature"];
 
@@ -38,17 +37,20 @@ export class Webhooks {
       try {
         switch (event.type) {
           case "checkout.session.completed":
-            await Payment.handlePaymentSuccess(res, event.data.object);
+            await Payment.handlePaymentSuccess(event.data.object, session);
             break;
 
           case "checkout.session.expired":
             break;
           case "payment_intent.payment_failed":
-            await Payment.handlePaymentFailure(res, event.data.object);
+            await Payment.handlePaymentFailure(event.data.object, session);
             break;
           default:
             break;
         }
+
+        await session.commitTransaction();
+        session.endSession();
 
         return ApiResponse.success(
           res,
@@ -58,10 +60,14 @@ export class Webhooks {
         );
       } catch (err) {
         console.error(err);
+        await session.abortTransaction();
+        session.endSession();
         return ApiResponse.error(res, "Webhook Failed", 500, err.message);
       }
     } catch (error) {
       console.error("Error in Stripe webhook handler:", error);
+      await session.abortTransaction();
+      session.endSession();
       if (error instanceof ApiError) {
         return ApiResponse.error(
           res,
--- routes/payment_route.js
+++ routes/payment_route.js
@@ -10,6 +10,9 @@ router.get("/booking/:booking_id/payment/active-session", User.isLoggedIn, Payme
 // Find if there's an active payment session for the booking ticket
 router.get("/booking/session/success", Payment.getSessionInformation);
 
+// Get Payment Logs
+router.get("/booking/:booking_id/payment/logs", User.isLoggedIn, Payment.getPaymentLogs);
+
 // Cancel Payment Session
 router.patch("/booking/:booking_id/payment/close", User.isLoggedIn, Payment.cancelPaymentSession);
 
